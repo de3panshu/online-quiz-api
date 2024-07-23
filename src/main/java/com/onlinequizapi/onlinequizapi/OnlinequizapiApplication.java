3328ba6bee25c414959e2b5d16b95936c8182b4c
Deepanshu <sahu.deeepanshu.united@gmail.com>
2024-07-23 08:03:11 +0530

    initial setup done

---
+++ src/main/java/com/onlinequizapi/onlinequizapi/OnlinequizapiApplication.java
@@ -0,0 +1,13 @@
+package com.onlinequizapi.onlinequizapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinequizapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlinequizapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onlinequizapi/onlinequizapi/OnlinequizapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlinequizapi.onlinequizapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinequizapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
